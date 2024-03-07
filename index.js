be1f4ad3be27c209cfdb1d6eed0a284247873dde
luiscorrea8 <luis.correa8@unmsm.edu.pe>
2024-03-07 08:25:43 -0500

    nuevo cambio

--- index.js
+++ index.js
@@ -41,3 +41,17 @@ var visualization3 = d3plus
   .x('year')
   .y('value')
   .draw();
+
+
+  var visualization4 = d3plus
+  .viz()
+  .container('#viz4')
+  .data([
+    { year: 1991, name: 'alpha', value: 30 },
+    { year: 1992, name: 'alpha', value: 20 },
+  ])
+  .type('bar')
+  .id('name')
+  .x('year')
+  .y('value')
+  .draw();
